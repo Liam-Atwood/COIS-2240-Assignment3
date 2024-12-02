192b5c773d6c36110115044b57e0bf88a180a298
Liam-Atwood <liama00@icloud.com>
2024-12-01 21:29:00 -0500

    Task 3.2 Add testing for borrow/return system

--- Library/src/LibraryManagementTest.java
+++ Library/src/LibraryManagementTest.java
@@ -1,17 +1,26 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LibraryManagementTest {
+	private Transaction transaction;
+	
+	@Before
+	public void setUp() {
+		transaction = Transaction.getTransaction();
+	}
 
 	@Test
 	public void testBookId() throws Exception {
+		// Add IDs within bounds
 		Book book1 = new Book(100, "Valid Book1");
 		assertEquals(100, book1.getId());
 		
 		Book book2 = new Book(999, "Valid Book2");
 		assertEquals(999, book2.getId());
 		
+		// Add books outside of bounds
 		Book book3;
 		try {
 			book3 = new Book(1000, "Invalid Book1");
@@ -32,5 +41,33 @@ public class LibraryManagementTest {
 		} catch (Exception e) {
 			assertEquals("Id 2000 is invalid. Must be between 100 and 999", e.getMessage());
 		}
+	}
+	
+	@Test
+	public void testBorrowReturn() throws Exception {
+		Book book = new Book(100, "Test Book");
+		Member member = new Member(1, "Test Member");
+		// Check book exists
+		assertTrue(book.isAvailable());
+		
+		boolean firstBorrow = transaction.borrowBook(book, member);
+		// Borrow successful
+		assertTrue(firstBorrow);
+		// Book unavailable
+		assertFalse(book.isAvailable());
+		
+		boolean secondBorrow = transaction.borrowBook(book, member);
+		// Borrow attempt unsuccessful
+		assertFalse(secondBorrow);
+		
+		boolean firstReturn = transaction.returnBook(book, member);
+		// Return successful
+		assertTrue(firstReturn);
+		// Book available
+		assertTrue(book.isAvailable());
+		
+		boolean secondReturn = transaction.returnBook(book, member);
+		// Return failed
+		assertFalse(secondReturn);
 	}
 }
